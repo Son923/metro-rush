f059f05c1bd5aa7cc27691a1cf4d84d7d4c2b170
unknown <thaison1804@gmail.com>
2019-01-05 04:10:41 +0700

    clean up note.py

--- note.py
+++ note.py
@@ -14,23 +14,3 @@ ratio = turns_A_reach_end?? / turns_A_wait??
 Conclusion:
 Find path for 1st train so that last_train's turn is smallest (minimum of line changes)
 total_turn = turns_last_train_wait + turns_that_last_train_reach_end
-
-
-def parse_file(filename):
-    return data 
-
-def create_nodes(data):
-    return nodes
-
-def create_lines(nodes):
-    return lines 
-
-def create_graph(lines):
-    return graph
-
-def main():
-    nodes = create_nodes(data)
-    lines = create_lines(nodes)
-    graph = create_graph(line)
-    result = execute_algo(graph)
-    return result
